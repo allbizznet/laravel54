1f1f203f4723ec152eb84d2cf4c9a4c3e83486d2
All BizzNet <allbizznet@yahoo.fr>
2017-05-12 09:41:53 +0200

    version1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
                 <div class="col-sm-3 site-team-corps">
                     <img src="img/img-lee-palmer.jpg" class="img-responsive"/>
                     <h3>Lee Palmer</h3>
-                    <h4>EAMER Regional Business Manager</h4>
+                    <h4>EAMER - General manager Imaging and Vice President, EAMER</h4>
                     <a href="mailto:lee.palme@kodakalaris.com"><div class="site-team-email"><span class="glyphicon glyphicon-envelope"></span> lee.palmer@kodakalaris.com</div></a>
                     <div class="site-team-earphone"><span class="glyphicon glyphicon-earphone"></span> +44 (0)1442 846771</span></div>
                     <div class="site-team-phone"><span class="glyphicon glyphicon-phone"></span> +44 (0)7740 096717</span></div>
@@ -47,7 +47,7 @@
                 <div class="col-sm-3 site-team-corps">
                     <img src="img/img-previna-mistry.jpg" class="img-responsive"/>
                     <h3>Previna Mistry</h3>
-                    <h4>Executive Assistant</h4>
+                    <h4>EAMER - Executive Assistant</h4>
                     <a href="mailto:previna.mistry@kodakalaris.com"><div class="site-team-email"><span class="glyphicon glyphicon-envelope"></span> previna.mistry@kodakalaris.com</div></a>
                     <div class="site-team-earphone"><span class="glyphicon glyphicon-earphone"></span> +44 (0)1442 846705</span></div>
                     <div class="site-team-phone"><span class="glyphicon glyphicon-phone"></span> +44 (0)7946 002435</span></div>
@@ -56,7 +56,7 @@
                 <div class="col-sm-3 site-team-corps">
                     <img src="img/img-helena-babic.jpg" class="img-responsive"/>
                     <h3>Helena Babic</h3>
-                    <h4>Head of Marketing DACH &amp; PR EMEAR</h4>
+                    <h4>EAMER/DACH - Head of Marketing DACH &amp; PR EAMER</h4>
                     <a href="mailto:helena.babic@kodakalaris.com"><div class="site-team-email"><span class="glyphicon glyphicon-envelope"></span> helena.babic@kodakalaris.com</a></div></a>
                     <div class="site-team-earphone"><span class="glyphicon glyphicon-earphone"></span> +49 (0)711 2528 1841</span></div>
                     <div class="site-team-phone"><span class="glyphicon glyphicon-phone"></span> +49 (0)170 4567 029</span></div>
@@ -65,7 +65,7 @@
                 <div class="col-sm-3 site-team-corps">
                     <img src="img/img-lars-fiedler.jpg" class="img-responsive"/>
                     <h3>Lars Fiedler</h3>
-                    <h4>Director Product Marketing</h4>
+                    <h4>EAMER - Director Product Marketing</h4>
                     <a href="mailto:lars.fiedler@kodakalaris.com"><div class="site-team-email"><span class="glyphicon glyphicon-envelope"></span> lars.fiedler@kodakalaris.com</div></a>
                     <div class="site-team-earphone"><span class="glyphicon glyphicon-earphone"></span> +49 (0)711 2528 1858</div>
                     <div class="site-team-phone"><span class="glyphicon glyphicon-phone"></span> +49 (0)170 4567847</div>
@@ -74,42 +74,24 @@
                 <div class="col-sm-3 site-team-corps">
                     <img src="img/img-claire-jussiaume.jpg" class="img-responsive"/>
                     <h3>Claire Jussiaume</h3>
-                    <h4>EMEER</h4>
+                    <h4>EAMER - Marcoms</h4>
                     <a href="mailto:claire.jussiaume@kodakalaris.com"><div class="site-team-email"><span class="glyphicon glyphicon-envelope"></span> claire.jussiaume@kodakalaris.com</div></a>
                     <div class="site-team-earphone"><span class="glyphicon glyphicon-earphone"></span> +33 (0)1 76 46 00 31</div>
                     <div class="site-team-phone"><span class="glyphicon glyphicon-phone"></span> +33 (0)6 82 96 64 70</div>
                     <div class="site-team-map"><span class="glyphicon glyphicon-map-marker"></span> FR - Maisons Alfort</div>
                 </div>
                 <div class="col-sm-3 site-team-corps">
-                    <img src="img/img-alfred-otieno.jpg" class="img-responsive"/>
-                    <h3>Paul Sheperd Babic</h3>
-                    <h4>EMEER</h4>
-                    <a href="mailto:@kodakalaris.com"><div class="site-team-email"><span class="glyphicon glyphicon-envelope"></span>  </div></a>
-                    <div class="site-team-earphone"><span class="glyphicon glyphicon-earphone"></span> </div>
-                    <div class="site-team-phone"><span class="glyphicon glyphicon-phone"></span> </div>
-                    <div class="site-team-map"><span class="glyphicon glyphicon-map-marker"></span> </div>
+                    <img src="img/img-paul-shepherd.jpg" class="img-responsive"/>
+                    <h3>Paul Sheperd</h3>
+                    <h4>EAMER - Service Director</h4>
+                    <a href="mailto:paul.shepherd@kodakalaris.com"><div class="site-team-email"><span class="glyphicon glyphicon-envelope"></span> paul.shepherd@kodakalaris.com</div></a>
+                    <div class="site-team-earphone"><span class="glyphicon glyphicon-earphone"></span> +33 (0)1 76 46 00 60</div>
+                    <div class="site-team-phone"><span class="glyphicon glyphicon-phone"></span> +33 (0)6 07 45 13 93</div>
+                    <div class="site-team-map"><span class="glyphicon glyphicon-map-marker"></span> FR/DE - Maisons Alfort/Stuttgart</div>
                 </div>
-                <div class="col-sm-3 site-team-corps">
-                    <img src="img/img-alfred-otieno.jpg" class="img-responsive"/>
-                    <h3>Wolfgang Venzi</h3>
-                    <h4>Business Director DACH</h4>
-                    <a href="mailto:@kodakalaris.com"><div class="site-team-email"><span class="glyphicon glyphicon-envelope"></span>  </div></a>
-                    <div class="site-team-earphone"><span class="glyphicon glyphicon-earphone"></span> </div>
-                    <div class="site-team-phone"><span class="glyphicon glyphicon-phone"></span> </div>
-                    <div class="site-team-map"><span class="glyphicon glyphicon-map-marker"></span> </div>
-                </div>
-            </div>
-            <div class="row" >
-                <hr />                
-            </div>
-            <div class="row site-team-titre" id="sales-team">
-                <ul><li>Sales Team</li></ul>
-                <span class="glyphicon glyphicon-menu-right"></span>
-            </div>
-            <div class="row">
                 <div class="col-sm-3 site-team-corps">
                     <div class="hovereffect">
-                        <img src="img/img-alfred-otieno.jpg" class="img-responsive"/>
+                        <img src="img/img-wolfgang-venzl.jpg" class="img-responsive"/>
                         <div class="overlay">
                             <p><a href="#">Management Team</a></p>
                             <p><a href="#">Sales Team</a></p>
@@ -117,12 +99,77 @@
                             <p><a href="#">DACH Team</a></p>
                         </div>
                     </div>
-                    <h3>Helena Babic</h3>
-                    <h4>EMEER</h4>
-                    <a href="mailto:@kodakalaris.com"><div class="site-team-email"><span class="glyphicon glyphicon-envelope"></span>  </div></a>
-                    <div class="site-team-earphone"><span class="glyphicon glyphicon-earphone"></span> </div>
-                    <div class="site-team-phone"><span class="glyphicon glyphicon-phone"></span> </div>
-                    <div class="site-team-map"><span class="glyphicon glyphicon-map-marker"></span> </div>
+                    <h3>Wolfgang Venzi</h3>
+                    <h4>EAMER - Sales Director</h4>
+                    <a href="mailto:wolfgang.venzl@kodakalaris.com"><div class="site-team-email"><span class="glyphicon glyphicon-envelope"></span> wolfgang.venzl@kodakalaris.com</div></a>
+                    <div class="site-team-earphone"><span class="glyphicon glyphicon-earphone"></span> +49 (0)711 2528 1913</div>
+                    <div class="site-team-phone"><span class="glyphicon glyphicon-phone"></span> +49 (0)170 4567802</div>
+                    <div class="site-team-map"><span class="glyphicon glyphicon-map-marker"></span> DE - Stuttgart</div>
+                </div>
+            </div>
+            <div class="row" >
+                <hr />                
+            </div>
+            <div class="row site-team-titre" id="sales-team">
+                <ul><li>Sales Team</li></ul>
+            </div>
+            <div class="row">
+                <div class="col-sm-3 site-team-corps">
+                    <div class="hovereffect">
+                        <img src="img/img-wolfgang-venzl.jpg" class="img-responsive"/>
+                        <div class="overlay">
+                            <p><a href="#">Management Team</a></p>
+                            <p><a href="#">Sales Team</a></p>
+                            <p><a href="#">Sales team management</a></p>
+                            <p><a href="#">DACH Team</a></p>
+                        </div>
+                    </div>
+                    <h3>Wolfgang Venzi</h3>
+                    <h4>EAMER - Sales Director</h4>
+                    <a href="mailto:wolfgang.venzl@kodakalaris.com"><div class="site-team-email"><span class="glyphicon glyphicon-envelope"></span> wolfgang.venzl@kodakalaris.com</div></a>
+                    <div class="site-team-earphone"><span class="glyphicon glyphicon-earphone"></span> +49 (0)711 2528 1913</div>
+                    <div class="site-team-phone"><span class="glyphicon glyphicon-phone"></span> +49 (0)170 4567802</div>
+                    <div class="site-team-map"><span class="glyphicon glyphicon-map-marker"></span> DE - Stuttgart</div>
+                </div>
+            </div>
+            <div class="row site-team-sous-titre" id="sales-team">                
+                <span class="glyphicon glyphicon-menu-right"></span> Sales team management
+            </div>
+            <div class="row">
+                <div class="col-sm-3 site-team-corps">
+                    <div class="hovereffect">
+                        <img src="img/img-wolfgang-venzl.jpg" class="img-responsive"/>
+                        <div class="overlay">
+                            <p><a href="#">Management Team</a></p>
+                            <p><a href="#">Sales Team</a></p>
+                            <p><a href="#">Sales team management</a></p>
+                            <p><a href="#">DACH Team</a></p>
+                        </div>
+                    </div>
+                    <h3>Wolfgang Venzi</h3>
+                    <h4>EAMER - Sales Director</h4>
+                    <a href="mailto:wolfgang.venzl@kodakalaris.com"><div class="site-team-email"><span class="glyphicon glyphicon-envelope"></span> wolfgang.venzl@kodakalaris.com</div></a>
+                    <div class="site-team-earphone"><span class="glyphicon glyphicon-earphone"></span> +49 (0)711 2528 1913</div>
+                    <div class="site-team-phone"><span class="glyphicon glyphicon-phone"></span> +49 170 4567802</div>
+                    <div class="site-team-map"><span class="glyphicon glyphicon-map-marker"></span> DE - Stuttgart</div>
+                </div>
+                <div class="col-sm-3 site-team-corps">                    
+                    <img src="img/img-paul-austin.jpg" class="img-responsive"/>                        
+                    <h3>Paul Austin</h3>
+                    <h4>EAMER - Business Director - UK/FR/BL/MED</h4>
+                    <a href="mailto:paul.austin@kodakalaris.com"><div class="site-team-email"><span class="glyphicon glyphicon-envelope"></span> paul.austin@kodakalaris.com</div></a>
+                    <div class="site-team-earphone"><span class="glyphicon glyphicon-earphone"></span> +44 (0)1442 846782</div>
+                    <div class="site-team-phone"><span class="glyphicon glyphicon-phone"></span> +44 (0)7710 387285</div>
+                    <div class="site-team-map"><span class="glyphicon glyphicon-map-marker"></span> UK - Hemel</div>
+                </div>
+                <div class="col-sm-3 site-team-corps">                    
+                    <img src="img/img-anja-gubensek.jpg" class="img-responsive"/>                        
+                    <h3>Anja Gubensek</h3>
+                    <h4>EMERGING - Business Director - Eastern Europe, Greater Russia, Middle East &amp; Africa</h4>
+                    <a href="mailto:anja.gubensek@kodakalaris.com"><div class="site-team-email"><span class="glyphicon glyphicon-envelope"></span> anja.gubensek@kodakalaris.com</div></a>
+                    <div class="site-team-earphone"><span class="glyphicon glyphicon-earphone"></span> + 43 (0)197001248</div>
+                    <div class="site-team-phone"><span class="glyphicon glyphicon-phone"></span> +43 676 83 980 248</div>
+                    <div class="site-team-map"><span class="glyphicon glyphicon-map-marker"></span> Austria -  Home office</div>
                 </div>
             </div>
         </div>
