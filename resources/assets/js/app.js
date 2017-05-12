45f024f8f99bfb8f75cdf3369545e6e6bf5d38d9
All BizzNet <allbizznet@yahoo.fr>
2017-05-12 08:55:25 +0200

    Modification header

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,6 +4,8 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
+window.$ = window.jQuery = require('jquery');
+window.Laravel = { csrfToken: $('meta[name=csrf-token]').attr("content") };
 
 require('./bootstrap');
 
@@ -14,9 +16,35 @@ window.Vue = require('vue');
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-
+/*
 Vue.component('example', require('./components/Example.vue'));
 
 const app = new Vue({
     el: '#app'
+});
+*/
+
+
+/**
+ * Function Scroll UP
+ */
+$(document).ready(function(){
+     $(window).scroll(function () {
+            if ($(this).scrollTop() > 50) {
+                $('#back-to-top').fadeIn();
+            } else {
+                $('#back-to-top').fadeOut();
+            }
+        });
+        // scroll body to 0px on click
+        $('#back-to-top').click(function () {
+            $('#back-to-top').tooltip('hide');
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+        
+        $('#back-to-top').tooltip('show');
+
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,91 +5,143 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <title>{{ config('app.name') }}</title>
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link rel="stylesheet" type="text/css" href="/css/app.css">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
+        <header>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <img src="/img/logo-kodak-alaris.png" class="img-responsive" alt="Logo Kodak Alaris" />
+                        <p>EAMER Sales and Marketing Who's Who</p>
+                    </div>
+                    <div class="col-md-push-5 col-sm-3">
+                        <ul>
+                            <li><a href="#management-team">Management Team</a></li>
+                            <li><a href="#sales-team">sales Team</a></li>
+                            <li><a href="#product-marketing">Product marketing</a></li>
+                            <li><a href="#dach-trade+pr-eamer-team">Dach trade + pr eamer team</a></li>
+                            <li><a href="#service-supports">Service &amp; Supports</a></li>
+                        </ul>
+                    </div>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+            </div>
+        </header>
+        <div class="container">
+            <div class="row site-team-titre" id="management-team">
+                <ul><li>Management Team</li></ul>
+            </div>
+            <div class="row">
+                <div class="col-sm-3 site-team-corps">
+                    <img src="img/img-lee-palmer.jpg" class="img-responsive"/>
+                    <h3>Lee Palmer</h3>
+                    <h4>EAMER Regional Business Manager</h4>
+                    <a href="mailto:lee.palme@kodakalaris.com"><div class="site-team-email"><span class="glyphicon glyphicon-envelope"></span> lee.palmer@kodakalaris.com</div></a>
+                    <div class="site-team-earphone"><span class="glyphicon glyphicon-earphone"></span> +44 (0)1442 846771</span></div>
+                    <div class="site-team-phone"><span class="glyphicon glyphicon-phone"></span> +44 (0)7740 096717</span></div>
+                    <div class="site-team-map"><span class="glyphicon glyphicon-map-marker"></span> UK - Hemel</span></div>
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="col-sm-3 site-team-corps">
+                    <img src="img/img-previna-mistry.jpg" class="img-responsive"/>
+                    <h3>Previna Mistry</h3>
+                    <h4>Executive Assistant</h4>
+                    <a href="mailto:previna.mistry@kodakalaris.com"><div class="site-team-email"><span class="glyphicon glyphicon-envelope"></span> previna.mistry@kodakalaris.com</div></a>
+                    <div class="site-team-earphone"><span class="glyphicon glyphicon-earphone"></span> +44 (0)1442 846705</span></div>
+                    <div class="site-team-phone"><span class="glyphicon glyphicon-phone"></span> +44 (0)7946 002435</span></div>
+                    <div class="site-team-map"><span class="glyphicon glyphicon-map-marker"></span> UK - Hemel</span></div>
+                </div>
+                <div class="col-sm-3 site-team-corps">
+                    <img src="img/img-helena-babic.jpg" class="img-responsive"/>
+                    <h3>Helena Babic</h3>
+                    <h4>Head of Marketing DACH &amp; PR EMEAR</h4>
+                    <a href="mailto:helena.babic@kodakalaris.com"><div class="site-team-email"><span class="glyphicon glyphicon-envelope"></span> helena.babic@kodakalaris.com</a></div></a>
+                    <div class="site-team-earphone"><span class="glyphicon glyphicon-earphone"></span> +49 (0)711 2528 1841</span></div>
+                    <div class="site-team-phone"><span class="glyphicon glyphicon-phone"></span> +49 (0)170 4567 029</span></div>
+                    <div class="site-team-map"><span class="glyphicon glyphicon-map-marker"></span> DE - Stuttgart</span></div>
+                </div>
+                <div class="col-sm-3 site-team-corps">
+                    <img src="img/img-lars-fiedler.jpg" class="img-responsive"/>
+                    <h3>Lars Fiedler</h3>
+                    <h4>Director Product Marketing</h4>
+                    <a href="mailto:lars.fiedler@kodakalaris.com"><div class="site-team-email"><span class="glyphicon glyphicon-envelope"></span> lars.fiedler@kodakalaris.com</div></a>
+                    <div class="site-team-earphone"><span class="glyphicon glyphicon-earphone"></span> +49 (0)711 2528 1858</div>
+                    <div class="site-team-phone"><span class="glyphicon glyphicon-phone"></span> +49 (0)170 4567847</div>
+                    <div class="site-team-map"><span class="glyphicon glyphicon-map-marker"></span> DE - Stuttgart</div>
+                </div>
+                <div class="col-sm-3 site-team-corps">
+                    <img src="img/img-claire-jussiaume.jpg" class="img-responsive"/>
+                    <h3>Claire Jussiaume</h3>
+                    <h4>EMEER</h4>
+                    <a href="mailto:claire.jussiaume@kodakalaris.com"><div class="site-team-email"><span class="glyphicon glyphicon-envelope"></span> claire.jussiaume@kodakalaris.com</div></a>
+                    <div class="site-team-earphone"><span class="glyphicon glyphicon-earphone"></span> +33 (0)1 76 46 00 31</div>
+                    <div class="site-team-phone"><span class="glyphicon glyphicon-phone"></span> +33 (0)6 82 96 64 70</div>
+                    <div class="site-team-map"><span class="glyphicon glyphicon-map-marker"></span> FR - Maisons Alfort</div>
+                </div>
+                <div class="col-sm-3 site-team-corps">
+                    <img src="img/img-alfred-otieno.jpg" class="img-responsive"/>
+                    <h3>Paul Sheperd Babic</h3>
+                    <h4>EMEER</h4>
+                    <a href="mailto:@kodakalaris.com"><div class="site-team-email"><span class="glyphicon glyphicon-envelope"></span>  </div></a>
+                    <div class="site-team-earphone"><span class="glyphicon glyphicon-earphone"></span> </div>
+                    <div class="site-team-phone"><span class="glyphicon glyphicon-phone"></span> </div>
+                    <div class="site-team-map"><span class="glyphicon glyphicon-map-marker"></span> </div>
+                </div>
+                <div class="col-sm-3 site-team-corps">
+                    <img src="img/img-alfred-otieno.jpg" class="img-responsive"/>
+                    <h3>Wolfgang Venzi</h3>
+                    <h4>Business Director DACH</h4>
+                    <a href="mailto:@kodakalaris.com"><div class="site-team-email"><span class="glyphicon glyphicon-envelope"></span>  </div></a>
+                    <div class="site-team-earphone"><span class="glyphicon glyphicon-earphone"></span> </div>
+                    <div class="site-team-phone"><span class="glyphicon glyphicon-phone"></span> </div>
+                    <div class="site-team-map"><span class="glyphicon glyphicon-map-marker"></span> </div>
+                </div>
+            </div>
+            <div class="row" >
+                <hr />                
+            </div>
+            <div class="row site-team-titre" id="sales-team">
+                <ul><li>Sales Team</li></ul>
+                <span class="glyphicon glyphicon-menu-right"></span>
+            </div>
+            <div class="row">
+                <div class="col-sm-3 site-team-corps">
+                    <div class="hovereffect">
+                        <img src="img/img-alfred-otieno.jpg" class="img-responsive"/>
+                        <div class="overlay">
+                            <p><a href="#">Management Team</a></p>
+                            <p><a href="#">Sales Team</a></p>
+                            <p><a href="#">Sales team management</a></p>
+                            <p><a href="#">DACH Team</a></p>
+                        </div>
+                    </div>
+                    <h3>Helena Babic</h3>
+                    <h4>EMEER</h4>
+                    <a href="mailto:@kodakalaris.com"><div class="site-team-email"><span class="glyphicon glyphicon-envelope"></span>  </div></a>
+                    <div class="site-team-earphone"><span class="glyphicon glyphicon-earphone"></span> </div>
+                    <div class="site-team-phone"><span class="glyphicon glyphicon-phone"></span> </div>
+                    <div class="site-team-map"><span class="glyphicon glyphicon-map-marker"></span> </div>
                 </div>
             </div>
         </div>
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12 text-right">
+                        &copy; 2017 Kodak Alaris Inc.<br />
+                        Confidential – Not to be shared in any kind of form outside the company<br />
+                        The Kodak trademark and trade dress are used under license from Eastman Kodak Company
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+<a id="back-to-top" href="#" class="btn btn-primary btn-lg back-to-top" role="button" title="Click to return on the top page" data-toggle="tooltip" data-placement="left"><span class="glyphicon glyphicon-chevron-up"></span></a>
+
+
+        <script src="{{asset('js/app.js')}}"></script>
+        <!-- <script src="/js/app.js"></script> -->
     </body>
 </html>
